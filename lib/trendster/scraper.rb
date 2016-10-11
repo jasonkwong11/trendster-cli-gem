2ab5ac686ae46ac17d6134b8fa63b2a47ce81b6c
Jason <jasonkwong11@gmail.com>
2016-10-11 15:50:33 -0400

    initial refactor of library events scraper

--- lib/trendster/scraper.rb
+++ lib/trendster/scraper.rb
@@ -1,39 +1,31 @@
 class Trendster::Scraper
 
-@@all = []
+  @@all = []
 
-attr_accessor :name, :description, :date, :location, :audience
+  attr_accessor :name, :description, :date, :location, :audience
   
-  def self.all
-    @@all
-  end
-
-  def self.scrape_library_page
-    url_endings = []
-    index_url = "http://cuyahogalibrary.org/Events/Event-Results.aspx"
-    index_doc = Nokogiri::HTML(open(index_url))
-      index_doc.css("article.sixcol").each do |event|
-        url_endings << event.css("a")[0]['href']
-      end
-    event_urls = []
-      url_endings.each do |url_ending|
-        event_urls << "http://cuyahogalibrary.org" + url_ending
-      end
-    event_doc_array = []
-      event_urls.each do |event_url|
-        event_doc = Nokogiri::HTML(open(event_url))
-       event_doc_array << event_doc
-      end
-      all_array = []
-    event_doc_array.each do |event_doc|
-     all_array << {
-      name: event_doc.css("h3").text,
-      description: event_doc.css("p.one-large-content").text || "",
-      date: event_doc.css("strong").text,
-      location: event_doc.css("p").css("a").text,
-      audience: event_doc.css("p").text.match("Adults") || event_doc.css("p").text.match("Children") 
-    }
+    def self.all
+      @@all
     end
-    all_array
-  end
+
+    def self.scrape_library_page
+      all_array = []
+      index_url = "http://cuyahogalibrary.org/Events/Event-Results.aspx"
+      index_doc = Nokogiri::HTML(open(index_url))
+        index_doc.css("article.sixcol").each do |event|
+          #a single url for each event...
+        event_url = "http://cuyahogalibrary.org" + event.css("a")[0]['href'] #a single url ending
+        #a single doc created for each event_url
+        event_doc = Nokogiri::HTML(open(event_url))
+       all_array << {
+        name: event_doc.css("h3").text,
+        description: event_doc.css("p.one-large-content").text || "",
+        date: event_doc.css("strong").text,
+        location: event_doc.css("p").css("a").text,
+        audience: event_doc.css("p").text.match("Adults") || event_doc.css("p").text.match("Children") 
+      }
+      end
+      all_array
+    end
+
 end
